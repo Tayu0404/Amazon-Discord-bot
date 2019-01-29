385701a9cb3a20ecddc9106154ac0eb811f058df
Tayu0404 <tkt.yusei0404@gmail.com>
2019-01-29 13:35:21 +0900

    1.0

--- main.py
+++ main.py
@@ -16,18 +16,29 @@ async def on_message(message):
         keyword = urllib.parse.quote(re.sub(r"!amazon search ","",(message.content).replace("\n"," ")))
         return_message = amazon.product_search(keyword)
         await client.send_message(message.channel, return_message)
-        """
-        if re.search(r"!amazon list ",message.content):
-            keyword = urllib.parse.quote(re.sub(r"!amazon list ","",(message.content).replace("\n"," ")))
-        if re.search(r"!amazon product ",message.content):
-            keyword = re.sub(r"!amazon product ","",(message.content))
-            keyword = urllib.parse.quote(re.sub(r"!amazon search ","",(message.content).replace("\n"," ")))
 
+    if re.search(r"!amazon list ",message.content):
+        keyword = urllib.parse.quote(re.sub(r"!amazon list ","",(message.content).replace("\n"," ")))
+        return_message = amazon.list(keyword,message.author.id)
+        await client.send_message(message.channel, return_message)
 
-        if re.search(r"!amazon help",message.content):
-            return_message = "Help\n```!amazon search keywords : 商品検索\n!amazon Number : 詳細検索```"
-            await client.send_message(message.channel, return_message)
-    if message.content.startswith("!amazon Bot Stop"):
-        client.logout()
-        """
+    if re.search(r"!amazon num ",message.content):
+        keyword = re.sub(r"!amazon num ","",message.content).replace("\n"," ")
+        return_message = amazon.direct_search(keyword,message.author.id)
+        await client.send_message(message.channel, return_message)
+    
+    if re.search(r"!amazon debug ",message.content):
+        if re.search(r"list",message.content):
+            return_message = amazon.show_list_debug(message.author.id)
+            await client.send_message(message.channel,return_message)
+
+    if re.search(r"!amazon help",message.content):
+        return_message = (
+                "```" + "\n" +
+                "search \"Keywords\" : Product Search" + "\n" +
+                "list \"Keywords\" : Product search and display list" +"\n" +
+                "num \"number\" : Product detail display" + "\n" +
+                "```"
+                )
+        await client.send_message(message.channel,return_message)
 client.run(os.environ['discord-token'])
