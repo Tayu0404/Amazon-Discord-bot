e40c6c338514e869580b5e1346fa3482afbd04e2
Tayu0404 <tkt.yusei0404@gmail.com>
2019-02-20 15:26:41 +0900

    fix

--- source/amazon_scraping.py
+++ source/amazon_scraping.py
@@ -2,35 +2,32 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import threading
-import time
 import re
 import urllib
-import schedule
 
 def get_html(url):
     print (url)
     options = Options()
-    options.add_argument("--headless")
-    options.add_argument("--no-sandbox")
-    driver = webdriver.Chrome(executable_path="./python/chromedriver",chrome_options=options)
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    driver = webdriver.Chrome(executable_path='./python/chromedriver',chrome_options=options)
     driver.get(url)
-    html = driver.page_source.encode("utf-8")
-    result = BeautifulSoup(html,"html.parser")
+    html = driver.page_source.encode('utf-8')
+    result = BeautifulSoup(html,'html.parser')
     return result
 
 class Amazon(object):
     def __init__(self):
         self.user_product = {}
-        self.stock_check_list = {}
         
     def list(self,keywords,user_id):
-        url = "https://www.amazon.co.jp/s/field-keywords=" + keywords
+        url = 'https://www.amazon.co.jp/s/field-keywords=' + keywords
         html = get_html(url)
-        list_temp = html.find("ul",id="s-results-list-atf")
-        result_num_list = re.findall(r"id=\"result_[^\"]*\"", str(list_temp))
+        list_temp = html.find('ul',id='s-results-list-atf')
+        result_num_list = re.findall(r'id=\'result_[^\']*\'', str(list_temp))
         num_list = []
         for i in result_num_list:
-            num_list.append(int(re.sub(r"\D","",i)))
+            num_list.append(int(re.sub(r'\D','',i)))
         num_max = None
         if max(num_list) > 10 :
             num_max = 10
@@ -38,115 +35,91 @@ class Amazon(object):
             num_max = (max(num_list))
         
         return_message_text = (
-                urllib.parse.unquote(keywords) + "の検索結果"
+                urllib.parse.unquote(keywords) + 'の検索結果'
                 )
         self.user_product[user_id] = {}
         for i in range(num_max):
-            count = "result_" + str(i)
-            result = list_temp.find("li",id=count)
-            product_id = result["data-asin"]
-            name = result.find("h2").text
-            price = re.sub("\s","",(result.find("span",class_="a-color-price").text)) 
+            count = 'result_' + str(i)
+            result = list_temp.find('li',id=count)
+            product_id = result['data-asin']
+            name = result.find('h2').text
+            price = re.sub('\s','',(result.find('span',class_='a-color-price').text)) 
             return_message_text = (
                     return_message_text +
-                    "\n" +
-                    str(i) + "\n" +
-                    name + "\n" +
-                    "```" + "\n" +
-                    price + "\n" +
-                    product_id + "\n" +
-                    "```"
+                    '\n' +
+                    str(i) + '\n' +
+                    name + '\n' +
+                    '```' + '\n' +
+                    price + '\n' +
+                    product_id + '\n' +
+                    '```'
                         )
             self.user_product[user_id].setdefault(str(i),product_id)
         print(self.user_product)
         return return_message_text
 
     def product(self,product_id):
-        url = "https://www.amazon.co.jp/dp/product/" + product_id
+        url = 'https://www.amazon.co.jp/dp/product/' + product_id
         html = get_html(url)
-        price = re.sub("\s","",(html.find("span",id="priceblock_ourprice").text))
-        details = re.sub(r"</?b>|詳細を見る","",(html.find("div",id="ddmDeliveryMessage").text)).strip()
-        ships = re.sub(r"\n","",(html.find("div",id="merchant-info").text)).strip()
-        name = html.find("span",id="productTitle").text.strip()
-        stock = re.sub(r"\n|在庫状況について","",(html.find("div",id="availability").text)).strip()
-        img_url = html.find("img",alt=name)["src"]
+        price = re.sub('\s','',(html.find('span',id='priceblock_ourprice').text))
+        details = re.sub(r'</?b>|詳細を見る','',(html.find('div',id='ddmDeliveryMessage').text)).strip()
+        ships = re.sub(r'\n','',(html.find('div',id='merchant-info').text)).strip()
+        name = html.find('span',id='productTitle').text.strip()
+        stock = re.sub(r'\n|在庫状況について','',(html.find('div',id='availability').text)).strip()
+        img_url = html.find('img',alt=name)['src']
         return_message_text = (
                 name +
-                "```" +
-                price + "\n" +
-                stock + "\n" +
-                details + "\n" + 
-                ships + "\n" + 
-                "```" +
-                img_url + "\n" +
+                '```' +
+                price + '\n' +
+                stock + '\n' +
+                details + '\n' + 
+                ships + '\n' + 
+                '```' +
+                img_url + '\n' +
                 url
                 )
         return return_message_text
 
     def direct_search(self,keywords,user_id):
-        url = "https://www.amazon.co.jp/dp/" + self.user_product[user_id][str(keywords)]
+        url = 'https://www.amazon.co.jp/dp/' + self.user_product[user_id][str(keywords)]
         html = get_html(url)
-        price = re.sub("\s","",(html.find("span",id="priceblock_ourprice").text))
-        details = re.sub(r"</?b>|詳細を見る","",(html.find("div",id="ddmDeliveryMessage").text)).strip()
-        ships = re.sub(r"\n","",(html.find("div",id="merchant-info").text)).strip()
-        name = html.find("span",id="productTitle").text.strip()
-        stock = re.sub(r"\n|在庫状況について","",(html.find("div",id="availability").text)).strip()
-        img_url = html.find("img",alt=name)["src"]
+        price = re.sub('\s','',(html.find('span',id='priceblock_ourprice').text))
+        details = re.sub(r'</?b>|詳細を見る','',(html.find('div',id='ddmDeliveryMessage').text)).strip()
+        ships = re.sub(r'\n','',(html.find('div',id='merchant-info').text)).strip()
+        name = html.find('span',id='productTitle').text.strip()
+        stock = re.sub(r'\n|在庫状況について','',(html.find('div',id='availability').text)).strip()
+        img_url = html.find('img',alt=name)['src']
         return_message_text = (
                 name +
-                "```" +
-                price + "\n" +
-                stock + "\n" +
-                details + "\n" + 
-                ships + "\n" + 
-                "```" +
-                img_url + "\n" +
+                '```' +
+
+                details + '\n' + 
+                ships + '\n' + 
+                '```' +
+                img_url + '\n' +
                 url
                 )
         return return_message_text
 
-    def product_search(self,keywords):
-        list_url = "https://www.amazon.co.jp/s/field-keywords=" + keywords
-        list_html = get_html(list_url)
-        product_id = list_html.find("li",id="result_0")["data-asin"]
-        return_message_text = self.product(product_id)
-        return return_message_text
-    
-    def show_list_debug(self,user_id):
-        user_list = self.user_product[user_id]
-        return_message_text = "User ID : " + user_id
-        for i in range(len(user_list)):
-            return_message_text = return_message_text + "\n" + str(i) + " : " + user_list[str(i)]
-        return return_message_text
-
-class periodically(object):
-    def __init__ (self):
-        self.key_list = {}
-
-    def list_set(self,user_id,key):
-        self.periodically_search_list[user_id] = {}
-        num = len(self.periodically_search_list[user_id])
-        self.periodically_search_list[user_id].setdefault(str(num),key)
-    
-    def search(self):
-        print("Search")
-        if len(self.key_list) > 0:
-            for i in key_list.keys():
-                for l in key_list[i].keys():
-                    print(l)
-
-    def seconds_time(self):
-          t = time.strptime(time.ctime())
-          print(time.strftime("%Y/%m/%d %H:%M:%S", t))
-
-    def check(self):
-        while True:
-            schedule.run_pending()
-            time.sleep(1)
-    
-    def check_start(self):
-        self.check_thread = threading.Thread(target=self.check)
-        schedule.every(1).minutes.do(self.search)
-        schedule.every(1).seconds.do(self.seconds_time)
-        self.check_thread.start()
-periodically().check_start()
+    def check_search(self,key):
+        url = 'https://www.amazon.co.jp/dp/product/' + key
+        html= get_html(url)
+        stock = re.sub(r'\n|在庫状況について','',(html.find('div',id='availability').text)).strip()
+        if re.search(r'在庫あり|残り',stock):
+            price = re.sub('\s','',(html.find('span',id='priceblock_ourprice').text))
+            details = re.sub(r'</?b>|詳細を見る','',(html.find('div',id='ddmDeliveryMessage').text)).strip()
+            ships = re.sub(r'\n','',(html.find('div',id='merchant-info').text)).strip()
+            name = html.find('span',id='productTitle').text.strip()
+            img_url = html.find('img',alt=name)['src']
+            return_message_text = (
+                    name +
+                    '```' +
+                    price + '\n' +
+                    stock + '\n' +
+                    details + '\n' + 
+                    ships + '\n' + 
+                    '```' +
+                    img_url + '\n' +
+                    url
+                    )
+            return return_message_text
--- source/main.py
+++ source/main.py
@@ -4,6 +4,9 @@ import amazon_scraping
 import re
 import urllib.parse
 import threading
+import schedule
+import time
+import asyncio
 
 client = discord.Client()
 amazon = amazon_scraping.Amazon()
@@ -11,40 +14,91 @@ amazon = amazon_scraping.Amazon()
 @client.event
 async def on_ready():
     print('ログインしました')
+    developer_id = await client.get_user_info('257025417490202625')
+    await client.send_message(developer_id,'Bot login')
+    check.check_start()
+
 
 @client.event
 async def on_message(message):
-    if message.content.startswith("!amazon search "):
-        keyword = urllib.parse.quote(re.sub(r"!amazon search ","",(message.content).replace("\n"," ")))
+    if message.content.startswith('!amazon search '):
+        keyword = urllib.parse.quote(re.sub(r'!amazon search ','',(message.content).replace('\n',' ')))
         return_message = amazon.product_search(keyword)
         await client.send_message(message.channel, return_message)
 
-    if re.search(r"!amazon list ",message.content):
-        keyword = urllib.parse.quote(re.sub(r"!amazon list ","",(message.content).replace("\n"," ")))
+    if re.search(r'!amazon list ',message.content):
+        keyword = urllib.parse.quote(re.sub(r'!amazon list ','',(message.content).replace('\n',' ')))
         return_message = amazon.list(keyword,message.author.id)
         await client.send_message(message.channel, return_message)
 
-    if re.search(r"!amazon num ",message.content):
-        keyword = re.sub(r"!amazon num ","",message.content).replace("\n"," ")
+    if re.search(r'!amazon num ',message.content):
+        keyword = re.sub(r'!amazon num ','',message.content).replace('\n',' ')
         return_message = amazon.direct_search(keyword,message.author.id)
         await client.send_message(message.channel, return_message)
     
-    if re.search(r"!amazon debug ",message.content):
-        if re.search(r"list",message.content):
+    if re.search(r'!amazon set ',message.content):
+        key = re.sub(r'!amazon set ','',(message.content))
+        user_id = message.author.id
+        check.list_set(user_id,key)
+        return_message = 'あとで考える'
+        await client.send_message(message.channel,return_message)
+
+    if re.search(r'!amazon debug ',message.content):
+        if re.search(r'list',message.content):
             return_message = amazon.show_list_debug(message.author.id)
             await client.send_message(message.channel,return_message)
-        if re.search(r"ping",message.content):
-            return_message = "pysm"
+        if re.search(r'ping',message.content):
+            return_message = 'pysm'
             await client.send_message(message.channel,return_message)
+        if re.search(r'check',message.content):
+            check.search()
+        if re.search(r'user_check',message.content):
+            check.list_check()
 
-    if re.search(r"!amazon help",message.content):
+    if re.search(r'!amazon help',message.content):
         return_message = (
-                "```" + "\n" +
-                "search \"Keywords\" : Product Search" + "\n" +
-                "list \"Keywords\" : Product search and display list" +"\n" +
-                "num \"number\" : Product detail display" + "\n" +
-                "```"
+                '```' + '\n' +
+                'search \'Keywords\' : Product Search' + '\n' +
+                'list \'Keywords\' : Product search and display list' +'\n' +
+                'num \'number\' : Product detail display' + '\n' +
+                '```'
                 )
         await client.send_message(message.channel,return_message)
 
+async def stock_send_message(user_id,return_message):
+    user_dm = await client.get_user_info(user_id)
+    print(user_dm)
+    await client.send_message(user_dm,return_message)
+
+class CheckStock(object):
+    def __init__(self):
+        self.list = {}
+
+    def list_set(self,user_id,key):
+        self.list[user_id] = {}
+        num = str(len(self.list[user_id]))
+        self.list[user_id].setdefault(num,key)
+
+    def search(self):
+        for user_id in self.list.keys():
+            for num in self.list[user_id].keys():
+                key = self.list[user_id][num]
+                return_message = amazon.check_search(key)
+                stock_send_message(user_id,return_message)
+
+    def list_check(self):
+        print(self.list)
+
+    def roop(self):
+        print('check start')
+        while True:
+            schedule.run_pending()
+            time.sleep(1)
+    
+    def check_start(self):
+        self.check_thread = threading.Thread(target=self.roop)
+        schedule.every(1).minutes.do(self.search)
+        self.check_thread.start()
+
+check = CheckStock()
 client.run(os.environ['discord-token'])
